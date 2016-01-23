edb221cde3d3c9f6beaa36701f95087660151c19
Kenzie Togami <ket1999@gmail.com>
2016-01-22 20:01:35 -0800

    Remove MIDDLE talons entirely

--- src/org/usfirst/frc/team5818/robot/RobotConstants.java
+++ src/org/usfirst/frc/team5818/robot/RobotConstants.java
@@ -9,14 +9,12 @@ import edu.wpi.first.wpilibj.Talon;
 public final class RobotConstants {
 
 	public static final Talon TALON_LEFT_FRONT = new Talon(0);
-	// public static final Talon TALON_LEFT_MIDDLE = new Talon(1);
 	public static final Talon TALON_LEFT_BACK = new Talon(1);
 
 	public static final Talon TALON_RIGHT_FRONT = new Talon(2);
-	// public static final Talon TALON_RIGHT_MIDDLE = new Talon(4);
 	public static final Talon TALON_RIGHT_BACK = new Talon(3);
 
-	public static final Talon TALON_ARM_MOTOR = new Talon(6);
+	public static final Talon TALON_ARM_MOTOR = new Talon(4);
 
 	public static final Joystick JOYSTICK_A = new Joystick(0);
 	public static final Joystick JOYSTICK_B = new Joystick(1);
