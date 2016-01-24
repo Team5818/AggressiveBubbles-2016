1e351f6fc4e3e8d44064e831846de5c0c33b15a8
Alexander Fell <afellrocks@gmail.com>
2016-01-23 22:07:23 -0800

    Arm class-move together
    Made the arm object have a right and a left sub-arm(still called arm..). Doesn't have PIDController so arms move at different rates. Have individual adjuster buttons for both arms.
    
    STABLE

--- src/org/usfirst/frc/team5818/robot/Arm.java
+++ src/org/usfirst/frc/team5818/robot/Arm.java
@@ -2,29 +2,36 @@ package org.usfirst.frc.team5818.robot;
 
 public class Arm {
 
-	private static final double MULTIPLIER = 0.5;
+    private static final double MULTIPLIER = -1.0;
     private double power;
     private double maxPower = MULTIPLIER;
     private double minPower = -MULTIPLIER;
 
     private double angle;
 
-	public Arm(){
-		RobotConstants.ARM_ENCODER.reset();
-		RobotConstants.ARM_ENCODER.setDistancePerPulse(RobotConstants.ARM_ENCODER_SCALE); // Angle per pulse in our case
-	}
+    public Arm(){
+	RobotConstants.ARM_ENCODER.reset();
+	RobotConstants.ARM_ENCODER.setDistancePerPulse(RobotConstants.ARM_ENCODER_SCALE); // Angle per pulse in our case
+    }
 
+   public Arm(Encoder e){
+		
+    }
+	
     /**
-     * Sets power of arm, keeping arm power within max and minimum parameters .5
+     * Sets power of left arm, keeping arm power within max and minimum parameters .5
      * and -.5
      * 
      * @param power
      *            - power value
      */
 
-    public void setPower(double power) {
-
-    	RobotConstants.TALON_ARM_MOTOR.set(power);
+    public void setPower(double power, char arm) {
+    	
+    	if(arm == 'r')
+    	RobotConstants.TALON_LEFT_ARM_MOTOR.set(power);
+    	if(arm == 'l')
+    	RobotConstants.TALON_RIGHT_ARM_MOTOR.set(power);
     }
 
     /**
@@ -64,9 +71,88 @@ public class Arm {
         return this.angle;
     }
     
+    public void aimAdjustLeft(Boolean up){
+    	if(up){
+    	/*	for(int i = 0; i < 101; i++)
+    			this.setPower(maxPower);
+    	}*/
+//    		double angl = this.getAngle();
+//    		DriverStation.reportError("" + angl + "\n", false);
+//    		while((angl - 400) != angl) {
+//    			this.setPower(maxPower);	
+//    		}
+    		this.setPower(maxPower * -.3, 'l');
+    		try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	if(!up){
+    	/*	for(int i = 0; i < 101; i++)
+    			this.setPower(minPower);
+    	*/
+//	    	double angl = this.getAngle();
+//    		DriverStation.reportError("" + angl + "\n", false);
+//			while((angl + 400) != angl) {
+//				this.setPower(minPower);
+//			}
+    		this.setPower(minPower * -.3, 'l');
+    		try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    }
+    
+    public void aimAdjustRight(Boolean up){
+    	if(up){
+    	/*	for(int i = 0; i < 101; i++)
+    			this.setPower(maxPower);
+    	}*/
+//    		double angl = this.getAngle();
+//    		DriverStation.reportError("" + angl + "\n", false);
+//    		while((angl - 400) != angl) {
+//    			this.setPower(maxPower);	
+//    		}
+    		this.setPower(maxPower * .3, 'r');
+    		try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	if(!up){
+    	/*	for(int i = 0; i < 101; i++)
+    			this.setPower(minPower);
+    	*/
+//	    	double angl = this.getAngle();
+//    		DriverStation.reportError("" + angl + "\n", false);
+//			while((angl + 400) != angl) {
+//				this.setPower(minPower);
+//			}
+    		this.setPower(minPower * .3, 'r');
+    		try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    }
+    
+    public void stabilize(){
+    	
+    }
+    
 	public void armTeleopPeriodic(){
 
-		this.setPower(-MULTIPLIER*RobotConstants.JOYSTICK_C.getY());
+		this.setPower(-MULTIPLIER*RobotConstants.JOYSTICK_C.getY(), 'l');
+		this.setPower(MULTIPLIER*RobotConstants.JOYSTICK_C.getY(), 'r');
 	}
 
 }
