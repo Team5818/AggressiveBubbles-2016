f5b2fb10fe9efbb257a81ab5a5f0a5e664ff8b90
Alexander Fell <afellrocks@gmail.com>
2016-01-23 22:11:55 -0800

    Updated in conjunction with arm
    Added constants for arm to access. Still need to adjust Left vs. Right variables as we are now basing on BACK VIEW of robot.
    
    STABLE

--- src/org/usfirst/frc/team5818/robot/RobotConstants.java
+++ src/org/usfirst/frc/team5818/robot/RobotConstants.java
@@ -16,7 +16,8 @@ public final class RobotConstants {
     public static final Talon TALON_RIGHT_FRONT = new Talon(2);
     public static final Talon TALON_RIGHT_BACK = new Talon(3);
 
-    public static final Talon TALON_ARM_MOTOR = new Talon(4);
+    public static final Talon TALON_LEFT_ARM_MOTOR = new Talon(4);
+    public static final Talon TALON_RIGHT_ARM_MOTOR = new Talon(5);
 
     public static final Joystick JOYSTICK_A = new Joystick(0);
     public static final Joystick JOYSTICK_B = new Joystick(1);
@@ -25,9 +26,16 @@ public final class RobotConstants {
     
     public static final Encoder ARM_ENCODER = new Encoder(0,1);
     public static final double ARM_ENCODER_SCALE = 1/375.0;
-	public static final int TRIGGER_BUTTON = ButtonType.kTrigger.value + 1;
-	public static final int ARM_RESET = 8;
-	public static final int PRINT_ANGLE = 7;
+    public static final int TRIGGER_BUTTON = ButtonType.kTrigger.value + 1;
+	
+    public static final int ARM_RESET = 8;
+    public static final int PRINT_ANGLE = 7;
+	
+    public static final int RIGHT_UP_ANGLE_BUTTON = 5;
+    public static final int RIGHT_DOWN_ANGLE_BUTTON = 3;
+	
+    public static final int LEFT_UP_ANGLE_BUTTON = 6;
+    public static final int LEFT_DOWN_ANGLE_BUTTON = 4;
 
     private RobotConstants() {
     }
