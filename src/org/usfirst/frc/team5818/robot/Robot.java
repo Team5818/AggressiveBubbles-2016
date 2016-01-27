bb0b5c7df9a59b4d240b1d5f04f2df88a07b3920
Zachary <zzackk125@gmail.com>
2016-01-26 20:32:11 -0800

    actually look

--- src/org/usfirst/frc/team5818/robot/Robot.java
+++ src/org/usfirst/frc/team5818/robot/Robot.java
@@ -42,6 +42,7 @@ public class Robot extends IterativeRobot {
         chooser.addDefault("Default Auto", defaultAuto);
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto choices", chooser);
+        new Eyes().LookAtMe();
     }
 
     /**
