efa4a02ff6cb3f7884c6ed0fc3921f9f23bdec25
Yoseph Sharif Alabdulwahab <yosephsa0@gmail.com>
2016-02-02 21:36:34 +0000

    Fixed incorrect implementation of interface, removed abstract identifiers in interface Module method declaration.

--- src/org/usfirst/frc/team5818/robot/modules/Module.java
+++ src/org/usfirst/frc/team5818/robot/modules/Module.java
@@ -12,19 +12,19 @@ public interface Module
     /**
      * Called first right before a robot enters a state. Such as teleopInit.
      */
-    public abstract void initModule();
+    public void initModule();
     
     /**
      * The periodic method for this module. Should not be used unless very necessary. Should have commands that can
      * achieve the function that this module should do.
      */
-    public abstract void teleopPeriodicModule();
+    public void teleopPeriodicModule();
     
     /**
      * This method is called when the robot is about to exit from a state. This is used to clear any variables that
      * need to be reset.
      */
-    public abstract void endModule();
+    public void endModule();
     
     
 }
