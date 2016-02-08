65b60f9bf2dbcf4087f29f1bd0b36f7a20568830
Yoseph Alabdulwahab <yosephsa0@gmail.com>
2016-02-07 21:26:34 -0800

    Added different Driving modes.
    Modified the DriveSide class such that there can be a powerLimit
    variable that limits the power output to the motors.
    Modified the DriveSide class such that there can be a cubeCurve to
    powering the motor.
    Modified the TankDriveCalculator such that it drives the robot with two
    sticks like a tank.
    Modified RobotDriver so that we can choose between different driving
    modes.
    Modified Vectors such that there can be inverted throttle and such that
    we can drive using thankdrive control.

--- src/org/usfirst/frc/team5818/robot/modules/drivetrain/TankDriveCalculator.java
+++ src/org/usfirst/frc/team5818/robot/modules/drivetrain/TankDriveCalculator.java
@@ -12,9 +12,10 @@ public enum TankDriveCalculator implements DriveCalculator {
     INSTANCE;
     
     public Vector2d compute(Vector2d in) {
-        double v = in.getY() * (1 - Math.abs(in.getY())) + in.getX();
-        double w = in.getY() * (1 - Math.abs(in.getX())) + in.getY();
-        return new Vector2d((v - w) / 2, (v + w) / 2);
+       
+        double leftMotorSpeed = in.getX();
+        double rightMotorSpeed = in.getY(); 
+        return new Vector2d(leftMotorSpeed, rightMotorSpeed);
     }
 
 }
