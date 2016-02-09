83128c72d1d46563d217b92fec8a195f100b8704
Kenzie Togami <ket1999@gmail.com>
2016-02-08 22:36:50 -0800

    Improved vector class

--- src/org/usfirst/frc/team5818/robot/util/Vectors.java
+++ src/org/usfirst/frc/team5818/robot/util/Vectors.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team5818.robot.util;
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
- * Helper class for vectors.s
+ * Helper class for vectors.
  */
 public final class Vectors {
 
@@ -12,16 +12,17 @@ public final class Vectors {
      * 
      * @param stick
      *            - The joystick to pull values from
-     * @param invertedThrottle Weather to invert throttle control similar to plane control.
+     * @param invertedThrottle
+     *            - Whether to invert throttle control similar to plane control.
      * 
      * @return A new vector of the values
      */
-    public static Vector2d fromJoystick(Joystick stick, boolean invertedThrottle) {
-        int sign = -1;
-        if(invertedThrottle)
-            sign = 1;
-        return new Vector2d(stick.getX(), sign*stick.getY());
+    public static Vector2d fromJoystick(Joystick stick,
+            boolean invertedThrottle) {
+        int sign = invertedThrottle ? -1 : 1;
+        return new Vector2d(stick.getX(), sign * -stick.getY());
     }
+
     /**
      * Creates a vector from the joystick axis values.
      * 
@@ -29,17 +30,17 @@ public final class Vectors {
      *            - The joystick to pull throttle values from
      * @param stick2
      *            - The joystick to pull turn values from
-     * @param invertedThrottle Weather to invert throttle control similar to plane control.
+     * @param invertedThrottle
+     *            - Whether to invert throttle control similar to plane control.
      * 
      * @return A new vector of the values
      */
-    public static Vector2d fromJoystick(Joystick stick, Joystick stick2, boolean invertedThrottle) {
-        int sign = -1;
-        if(invertedThrottle)
-            sign = 1;
-        return new Vector2d(stick2.getX(), sign*stick.getY());
+    public static Vector2d fromJoystick(Joystick stick, Joystick stick2,
+            boolean invertedThrottle) {
+        int sign = invertedThrottle ? -1 : 1;
+        return new Vector2d(stick2.getX(), sign * -stick.getY());
     }
-    
+
     /**
      * Creates a vector from the joystick axis values.
      * 
@@ -47,15 +48,15 @@ public final class Vectors {
      *            - The joystick to pull left motor throttle values from
      * @param stick2
      *            - The joystick to pull right motor throttle values from
-     * @param invertedThrottle Weather to invert throttle control similar to plane control.
+     * @param invertedThrottle
+     *            - Whether to invert throttle control similar to plane control.
      * 
      * @return A new vector of the values
      */
-    public static Vector2d fromJoystickTank(Joystick stick, Joystick stick2, boolean invertedThrottle) {
-        int sign = -1;
-        if(invertedThrottle)
-            sign = 1;
-        return new Vector2d(sign*stick.getY(), sign*stick2.getY());
+    public static Vector2d fromJoystickTank(Joystick stick, Joystick stick2,
+            boolean invertedThrottle) {
+        int sign = invertedThrottle ? -1 : 1;
+        return new Vector2d(sign * -stick.getY(), sign * -stick2.getY());
     }
 
     /**
