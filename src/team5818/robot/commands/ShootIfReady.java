0f9eceea55dc5c619a8d627153eb352f78f3a100
RivieraRobotics <rivieraroboticsteam@gmail.com>
2016-04-18 20:38:07 -0700

    Elongated auto aim timeout. Added shoot if ready command.

---
+++ src/team5818/robot/commands/ShootIfReady.java
@@ -0,0 +1,50 @@
+package team5818.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ShootIfReady extends Command {
+    private AutoAim aim;
+    private boolean finished = false;
+    private boolean hasStartedShooting = false;
+    private long time0;
+    
+    public ShootIfReady(AutoAim aim) {
+        this.aim = aim;
+    }
+    
+    public void execute() {
+        if(aim.foundTarget()) {
+            new Collect(1).start();
+            hasStartedShooting = true;
+            time0 = System.nanoTime();
+        }
+        
+        if(Math.abs(System.nanoTime() - time0) < (long)(0.5*1000000000)) {
+            finished = true;
+        }
+        
+    }
+    
+    public boolean isFinished() {
+        return isTimedOut() || finished;
+    }
+
+    @Override
+    protected void initialize() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void end() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void interrupted() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
